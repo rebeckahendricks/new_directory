e8cb653e4bfadabddd5bc24aa3d3fdbf576c4ec5
Rebecka Hendricks <rebecka.hendricks@gmail.com>
2022-06-08 11:50:19 -0400

    Restore original variables

--- arrays.rb
+++ arrays.rb
@@ -1,4 +1,3 @@
-# The pop method will delete the last element in my array (and return it). In this case, it will delete the "Mom" element.
 loves = ["Porter", "Corey", "Monty", "Mom"]
 ages = [0, 39, 4, 65]
 numbers = [11.11, 2.2, 3.3, 4.4]
