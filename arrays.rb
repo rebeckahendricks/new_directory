3e0706eb9378a9d63d24d1c6a6d443af96cc5986
Rebecka Hendricks <rebecka.hendricks@gmail.com>
2022-06-08 12:31:08 -0400

    Refine print problem

--- arrays.rb
+++ arrays.rb
@@ -4,16 +4,20 @@ numbers = [11.11, 2.2, 3.3, 4.4]
 skis = [false, true, false, false]
 
 # The pop method will delete the last element in the array. In this case it will delete the string element "Mom" and return "Mom".
-p loves.pop
+loves.pop
+p loves
 
 # The push method will add an element to the end of the array. In this case, I will add the integer element 36 to the end of the ages array. It will return all elements in the array.
-p ages.push(36)
+ages.push(36)
+p ages
 
 # The shift method will delete the first element in the array. In this case, it will delete the float element 11.11.
-p numbers.shift
+numbers.shift
+p numbers
 
 # The unshift method will add an element to the beginning of the array (and therefore shift all other elements one index position). In this case, it will add a true Boolean element at the beginning of the current array.
-p skis.unshift(true)
+skis.unshift(true)
+p skis
 
 # Index positions refer to the position an element is in the array, starting with 0.
 # For example, if I want to call the first element in the loves Array, I will have to use index position 0. (The output should be "Porter".)
